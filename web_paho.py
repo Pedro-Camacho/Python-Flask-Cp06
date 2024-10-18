f81659d923bd521ebe4cc2f21fd6d4fb908cb5bd
Pedro Camacho <pedroalmeidacamacho@gmail.com>
2024-10-18 13:05:42 -0300

    grafico luminosidade

--- web_paho.py
+++ web_paho.py
@@ -4,28 +4,50 @@ import paho.mqtt.client as mqtt
 import json
 
 # Constantes para MQTT
-MQTT_BROKER = "18.208.160.16"  # Substitua pelo endereço do seu broker
-MQTT_TOPIC = "/TEF/device001/attrs/p"  # Substitua pelo seu tópico
+MQTT_BROKER = "20.206.240.145"  # Substitua pelo endereço do seu broker
+MQTT_TOPIC = "/TEF/hosp200/attrs/l"  # Substitua pelo seu tópico
+MQTT_TOPIC_2 = "/TEF/hosp200/attrs/t"  # Substitua pelo seu tópico
+MQTT_TOPIC_3 = "/TEF/hosp200/attrs/h"  # Substitua pelo seu tópico
+
 
 # Inicializa o aplicativo Flask e o SocketIO
 app = Flask(__name__)
 socketio = SocketIO(app)
 
 # Variável global para armazenar o último valor do potenciômetro
-ultimo_valor = None
+ultimo_valor_luminosidade = None
+ultimo_valor_temperatura = None
+ultimo_valor_umidade = None
+
 
 # Funções de callback do MQTT
 def on_connect(client, userdata, flags, rc):
     print("Conectado com código de resultado " + str(rc))
     client.subscribe(MQTT_TOPIC)
+    client.subscribe(MQTT_TOPIC_2)
+    client.subscribe(MQTT_TOPIC_3)
 
 def on_message(client, userdata, msg):
     global ultimo_valor
     try:
+        #pegando informações luminosidade
         payload = json.loads(msg.payload)
-        ultimo_valor = payload  # Supondo que o payload seja um objeto JSON
-        socketio.emit('novo_dado', {'valor': ultimo_valor})  # Emite dados para o cliente
-        print(f"Mensagem recebida: {ultimo_valor}")  # Saída de depuração
+        ultimo_valor_luminosidade = payload  # Supondo que o payload seja um objeto JSON
+        socketio.emit('novo_dado_luminosidade', {'valor': ultimo_valor_luminosidade})  # Emite dados para o cliente
+        print(f"Mensagem recebida: {ultimo_valor_luminosidade}")  # Saída de depuração
+        
+        #pegando informações temperatura
+        payload = json.loads(msg.payload)
+        ultimo_valor_temperatura = payload  # Supondo que o payload seja um objeto JSON
+        socketio.emit('novo_dado_temperatura', {'valor': ultimo_valor_temperatura})  # Emite dados para o cliente
+        print(f"Mensagem recebida: {ultimo_valor_temperatura}")  # Saída de depuração
+        
+        #pegando informações luminosidade
+        payload = json.loads(msg.payload)
+        ultimo_valor_umidade = payload  # Supondo que o payload seja um objeto JSON
+        socketio.emit('novo_dado_umidade', {'valor': ultimo_valor_umidade})  # Emite dados para o cliente
+        print(f"Mensagem recebida: {ultimo_valor_umidade}")  # Saída de depuração
+        
     except json.JSONDecodeError:
         print("Falha ao decodificar JSON")
 
@@ -55,7 +77,7 @@ def index():
                     max-width: 400px;
                     margin: auto;
                 }
-                #valor-potenciometro {
+                #valor {
                     font-size: 48px; /* Tamanho da fonte aumentado */
                     font-weight: bold; /* Texto em negrito */
                 }
@@ -63,9 +85,9 @@ def index():
         </head>
         <body>
             <div class="container">
-                <h1 class="mt-5">Potenciômetro</h1>
+                <h1 class="mt-5">Luminosidade</h1>
                 <canvas id="gauge" width="400" height="400"></canvas>
-                <div id="valor-potenciometro" class="mt-3">
+                <div id="valor" class="mt-3">
                     Aguardando dados...
                 </div>
             </div>
@@ -77,7 +99,7 @@ def index():
                     data: {
                         labels: ['Valor', 'Restante'],
                         datasets: [{
-                            label: 'Valor do Potenciômetro',
+                            label: 'Valor',
                             data: [0, 100], // Valores iniciais (a serem atualizados)
                             backgroundColor: ['#36A2EB', '#E0E0E0'],
                             borderWidth: 1,
@@ -94,8 +116,8 @@ def index():
 
                 $(document).ready(function() {
                     var socket = io.connect('http://' + document.domain + ':' + location.port);
-                    socket.on('novo_dado', function(data) {
-                        $('#valor-potenciometro').text('Medição: ' + data.valor);
+                    socket.on('novo_dado_luminosidade', function(data) {
+                        $('#valor').text('Medição: ' + data.valor);
                         // Atualiza o gráfico gauge com o novo valor
                         gaugeChart.data.datasets[0].data[0] = data.valor; // Atualiza a parte do valor do gráfico de rosca
                         gaugeChart.data.datasets[0].data[1] = 100 - data.valor; // Parte restante (supondo que o valor máximo seja 100)
